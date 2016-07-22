ab3d79e704b0037d650674adb379ef26d414b819
Domen Kogler <domen.kogler@gmail.com>
2016-07-22 12:43:47 +0200

    push.bat

--- src/push.bat
+++ src/push.bat
@@ -1,1 +1,4 @@
+FOR /D %%D IN ("C:\Nuget\%1\*") DO rmdir "%%D" /s /q
+FOR /D %%D IN ("C:\Nuget\%1\*") DO rmdir c:\nuget\%1.%%~nxD\ /s /q
 call nuget push %1\bin\Release\*.nupkg -s C:\\Nuget
+FOR /D %%D IN (C:\Nuget\%1\*) DO xcopy c:\nuget\%1\%%~nxD\* c:\nuget\%1.%%~nxD\ /s /y
